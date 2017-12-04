8f8a7e37d2bf715989f6f30a6a7570e95f9d517c
Andreas Winge <github@winge.se>
2017-12-05 00:02:56 +0100

    Added tcl solution to day 4

---
+++ 4/4.tcl
@@ -0,0 +1,47 @@
+#!/usr/bin/tclsh
+
+proc check_valid _string {
+    set no_words        [llength [split $_string]]
+    set no_unique_words [llength [lsort -unique [split $_string]]]
+    if { $no_words == $no_unique_words } {
+	return 1
+    } else {
+	return 0
+    }
+}    
+
+set fp        [open "input" r]
+set file_data [read $fp]
+close $fp
+
+set rows [split $file_data "\n"]
+
+set valid   0
+set invalid 0
+foreach row $rows {
+    if { [check_valid $row] } {
+	incr valid
+    } else {
+	incr invalid
+    }
+}
+
+puts "Valid: $valid"
+puts "Invalid: $invalid"
+
+set valid   0
+set invalid 0
+foreach row $rows {
+    set new_row {}
+    foreach word [split $row] {
+	lappend new_row [join [lsort [split $word ""]] ""]
+    }
+    if { [check_valid $new_row] } {
+	incr valid
+    } else {
+	incr invalid
+    }
+}
+
+puts "Valid: $valid"
+puts "Invalid: $invalid"
