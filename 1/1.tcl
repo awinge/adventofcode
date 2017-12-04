53ae2439e10e763fac61523b82a168ea2b741eb0
Andreas Winge <github@winge.se>
2017-12-04 07:34:00 +0100

    Added second part to day 1 and refactoring to general solution

--- 1/1.tcl
+++ 1/1.tcl
@@ -1,19 +1,24 @@
 #!/usr/bin/tclsh
 
-proc sum_adjacent _list {
-    set _sum        0
-    set _last_digit [string index $_list end]
+proc sum_offset_match {_list _offset} {
+    set _sum 0
+    set _list_length [string length $_list]
+    set _current_index 0
     foreach _number [split $_list ""] {
-	if {$_last_digit == $_number} {
+	set _offset_index [expr ($_current_index + $_offset) % $_list_length]
+	if {$_number == [string index $_list $_offset_index]} {
 	    set _sum [expr $_sum + $_number]
 	}
-	set _last_digit $_number
+	incr _current_index
     }
-    return $_sum;
+    return $_sum
 }
 
 set fp        [open "input" r]
 set file_data [read $fp]
 close $fp
 
-puts "The answer is: [sum_adjacent $file_data]"
+puts "The answer is: [sum_offset_match $file_data 1]"
+
+set length [string length $file_data]
+puts "The answer for part two is: [sum_offset_match $file_data [expr $length / 2]]"
